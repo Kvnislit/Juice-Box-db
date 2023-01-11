6292985cf965c69f0fa57af2616322a0091181d8
Kevin Paucar <kpaucar05@gmail.com>
2023-01-10 20:11:28 -0600

    part 1

--- db/sql.sql
+++ db/sql.sql
@@ -4,12 +4,6 @@ CREATE TABLE users (
     name VARCHAR(50)
 );
 
-INSERT INTO juice( name) VALUES('foo');
-INSERT INTO juice( name) VALUES('bar');
-INSERT INTO juice( name) VALUES('bazz');
-INSERT INTO juice( name) VALUES('quq');
-INSERT INTO juice( name) VALUES('pop');
-
 
 SELECT id, name 
 FROM users;
