d0cf249db31f68cc45b75ccae30ae76dbdbae880
Kevin Paucar <kpaucar05@gmail.com>
2023-01-18 10:40:09 -0600

    upadte

--- api/users.js
+++ api/users.js
@@ -5,7 +5,17 @@ const usersRouter = express.Router();
 usersRouter.use((req, res, next) => {
   console.log("A request is being made to /users");
 
-  res.send({ message: 'hello from /users!' });
+  next(); // THIS IS DIFFERENT
+});
+
+const { getAllUsers } = require('../db');
+
+usersRouter.get('/', async (req, res) => {
+    const users = await getAllUsers();
+
+  res.send({
+    users: []
+  });
 });
 
 module.exports = usersRouter;
